c5fc4490497d81a1a4f93286ced04eb2bfd70d4a
Sofia Taipe <74900838@continental.edu.pe>
2023-09-14 17:50:04 -0500

    Primera version - Cuenta

---
+++ main.py
@@ -0,0 +1,10 @@
+from logica.Cuenta import Cuenta
+
+if __name__ == '__main__':
+    saldoInicial = float(input("Ingrese el saldo inicial: "))
+    cuenta = Cuenta(saldoInicial)
+    cuenta.interes()
+    cuenta.imprimerSaldo()
+
+
+
